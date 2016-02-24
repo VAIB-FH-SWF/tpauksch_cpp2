71528f9063d8766e504d82b4f166e1b8d6b59c93
Tim Pauksch <pauksch.tim@fh-swf.de>
2016-02-24 19:49:06 +0100

    Programmstruktur abgebildet.

---
+++ Aufgabe_1.1/src/editImg.cpp
@@ -0,0 +1,76 @@
+//=============================================================================
+//              Filename:   editImg.cpp
+//
+//          Beschreibung:   Programmierung mit C++ 2,
+//                          Aufgabe 1.1,
+//                          Verwendung von Strukturen.
+//
+//              Aufgabe:    1.1
+//
+//      Praktikumsgruppe:   [V-08:30] - Is-VAI-B1A
+//                Author:   Tim Pauksch
+//      Erstellungsdatum:   24.02.2016
+//    Letzte Anpassungen:   24.02.2016
+//
+//               Version:   1.0
+//              Compiler:   g++
+//=============================================================================
+#include <iostream>
+
+using namespace std;
+
+//=============================================================================
+// Definitionen
+//=============================================================================
+
+typedef unsigned int Pixel;
+
+//=============================================================================
+// Funktionen
+//=============================================================================
+
+void bild_lesen(){
+
+}
+
+void bild_schreiben(){
+
+}
+
+void delete_pixel_matrix(){
+
+}
+
+Pixel** new_pixel_matrix(){
+
+}
+
+void glaetten(){
+
+}
+
+void invertieren(){
+
+}
+
+void kantenbildung(){
+
+}
+
+void kopiere_bildkopf(){
+
+}
+
+void schwellwert(){
+
+}
+//=============================================================================
+// Hauptprogramm
+//=============================================================================
+
+int main() {
+
+   cout << "Aufgabe 1.1 - Verwendung von Strukturen." << endl;
+
+   return 0;
+}
