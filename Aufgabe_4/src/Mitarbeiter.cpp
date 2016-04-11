ea1fb06745bd97566dc49f1b984a02a4230ced3c
Tim Pauksch <pauksch.tim@fh-swf.de>
2016-04-11 21:15:46 +0200

    Erste Ansetze für Implementierung.

--- Aufgabe_4/src/Mitarbeiter.cpp
+++ Aufgabe_4/src/Mitarbeiter.cpp
@@ -44,11 +44,11 @@ class Mitarbeiter{
       void   print             ();
 
    //==============ACCESSORS=============================================================
-      string set_identnummer   (string value);
-      string set_nachname      (string value);
-      string set_vorname       (string value);
-      string set_abteilung     (string value);
-      string set_durchwahl     (string value);
+      void set_identnummer   (string value);
+      void set_nachname      (string value);
+      void set_vorname       (string value);
+      void set_abteilung     (string value);
+      void set_durchwahl     (string value);
 
    //==============OPERATORS=============================================================
       Mitarbeiter& operator = ( const Mitarbeiter &other);
@@ -57,3 +57,73 @@ class Mitarbeiter{
    //==============DATA MEMBERS==========================================================
       Person   *p;
 };
+
+string Mitarbeiter::get_identnummer () const {
+   return p->identnummer;
+}
+
+string Mitarbeiter::get_nachname () const {
+   return p->nachname;
+}
+
+string Mitarbeiter::get_vorname () const {
+   return p->vorname;
+}
+
+string Mitarbeiter::get_abteilung () const {
+   return p->abteilung;
+}
+
+string Mitarbeiter::get_durchwahl () const {
+   return p->durchwahl;
+}
+
+void Mitarbeiter::set_identnummer(string value){
+   p->identnummer = value;
+}
+
+void Mitarbeiter::set_nachname   (string value){
+   p->nachname    = value;
+}
+
+void Mitarbeiter::set_vorname    (string value){
+   p->vorname     = value;
+}
+
+void Mitarbeiter::set_abteilung  (string value){
+   p->abteilung   = value;
+}
+
+void Mitarbeiter::set_durchwahl  (string value){
+   p->durchwahl   = value;
+}
+
+Mitarbeiter::~Mitarbeiter(){
+   delete p;
+}
+
+void readData( string ifs_file_name ){
+   ifstream ifs;
+   ifs.open(ifs_file_name.c_str());
+
+   if (!ifs.is_open()) {
+      cerr << "\nERROR : Failed to open input file: " << ifs_file_name << endl;
+      exit(1);
+   }
+
+
+//   try{
+//      while(ifs >> cursor->identnummer >> cursor->nachname >> cursor->vorname >> cursor->abteilung >> cursor->durchwahl){
+//
+//      }
+//
+//   }catch(int e){
+//      cout << "\ERROR : Errorcode " << e << endl;
+//   }
+}
+
+int main() {
+
+
+   return 0;
+}
