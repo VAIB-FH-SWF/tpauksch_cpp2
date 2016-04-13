6f21656f656e921b483eec31bb248f6933506d45
Tim Pauksch <pauksch.tim@fh-swf.de>
2016-04-13 18:44:09 +0200

    Implementierung der Klasse aus der Aufgabe mit Testdaten.
    Läuft. Datensätze können Instanzen zugeordnet, ausgegeben und von Instanz zu Instanz kopiert werden.

--- Aufgabe_4/src/Mitarbeiter.cpp
+++ Aufgabe_4/src/Mitarbeiter.cpp
@@ -58,24 +58,55 @@ class Mitarbeiter{
       Person   *p;
 };
 
+Mitarbeiter::Mitarbeiter(string identnummer, string nachname, string vorname, string abteilung, string durchwahl){
+   p = new Person;
+   set_identnummer(identnummer);
+   set_nachname(nachname);
+   set_vorname(vorname);
+   set_abteilung(abteilung);
+   set_durchwahl(durchwahl);
+}
+
+Mitarbeiter::Mitarbeiter(const Mitarbeiter &other){
+   p = new Person;
+   p->identnummer = other.p->identnummer;
+   p->nachname    = other.p->nachname;
+   p->vorname     = other.p->vorname;
+   p->abteilung   = other.p->abteilung;
+   p->durchwahl   = other.p->durchwahl;
+}
+
+Mitarbeiter::~Mitarbeiter(){
+   delete p;
+}
+
 string Mitarbeiter::get_identnummer () const {
    return p->identnummer;
 }
 
-string Mitarbeiter::get_nachname () const {
+string Mitarbeiter::get_nachname    () const {
    return p->nachname;
 }
 
-string Mitarbeiter::get_vorname () const {
+string Mitarbeiter::get_vorname     () const {
    return p->vorname;
 }
 
-string Mitarbeiter::get_abteilung () const {
+string Mitarbeiter::get_abteilung   () const {
    return p->abteilung;
 }
 
-string Mitarbeiter::get_durchwahl () const {
+string Mitarbeiter::get_durchwahl   () const {
    return p->durchwahl;
+}
+
+void Mitarbeiter::print(){
+   cout  << left
+         << setw(9)  << get_identnummer()
+         << setw(20) << get_nachname()
+         << setw(20) << get_vorname()
+         << setw(5)  << get_abteilung()
+         << setw(5)  << get_durchwahl() << endl;
 }
 
 void Mitarbeiter::set_identnummer(string value){
@@ -98,32 +129,41 @@ void Mitarbeiter::set_durchwahl  (string value){
    p->durchwahl   = value;
 }
 
-Mitarbeiter::~Mitarbeiter(){
-   delete p;
-}
+Mitarbeiter& Mitarbeiter::operator = (const Mitarbeiter &other){
+   if (this != &other){
+         Person *save = p;
+         p = new Person;
+         p->identnummer = other.p->identnummer;
+         p->nachname    = other.p->nachname;
+         p->vorname     = other.p->vorname;
+         p->abteilung   = other.p->abteilung;
+         p->durchwahl   = other.p->durchwahl;
 
-void readData( string ifs_file_name ){
-   ifstream ifs;
-   ifs.open(ifs_file_name.c_str());
-
-   if (!ifs.is_open()) {
-      cerr << "\nERROR : Failed to open input file: " << ifs_file_name << endl;
-      exit(1);
+         delete save;
    }
-
-
-//   try{
-//      while(ifs >> cursor->identnummer >> cursor->nachname >> cursor->vorname >> cursor->abteilung >> cursor->durchwahl){
-//
-//      }
-//
-//   }catch(int e){
-//      cout << "\ERROR : Errorcode " << e << endl;
-//   }
+   return *this;
 }
 
 int main() {
+   Mitarbeiter m1;
+   m1.set_identnummer   ("10002220");
+   m1.set_nachname      ("JAYALATH");
+   m1.set_vorname       ("THILANAKA");
+   m1.set_abteilung     ("QC18");
+   m1.set_durchwahl     ("4701");
+   m1.print();
 
+   Mitarbeiter m2("10001222", "DOLBEL", "DAVID", "QC18", "4700");
+   m2.print();
+
+   Mitarbeiter m3(m1);
+
+   m1.print();
+   m3.print();
+
+   m1 = m2;
+
+   m1.print();
 
    return 0;
 }
